da1a8b562746d616e1c99f16ca3bfc39dc9d78fd
sebas <sebastianmq027@gmail.com>
2025-08-19 19:05:02 +0200

    commit portafolio

---
+++ src/data/technologies.ts
@@ -0,0 +1,24 @@
+interface Technology {
+  name: string;
+  icon: string;
+}
+
+export const TECHNOLOGIES: Technology[] = [
+  { name: 'React', icon: '/images/tech-icons/react.svg' },
+  { name: 'TypeScript', icon: '/images/tech-icons/typescript.svg' },
+  { name: 'Tailwind CSS', icon: '/images/tech-icons/tailwind.svg' },
+  { name: 'Python', icon: '/images/tech-icons/python.svg' },
+  { name: 'Git', icon: '/images/tech-icons/git.svg' },
+  { name: 'GitHub', icon: '/images/tech-icons/github.svg' },
+  { name: 'Vercel', icon: '/images/tech-icons/vercel.svg' },
+  { name: 'Figma', icon: '/images/tech-icons/figma.svg' },
+  { name: 'Javascript', icon: '/images/tech-icons/javascript.svg' },
+  { name: 'Java', icon: '/images/tech-icons/java.svg' },
+  { name: 'Mysql', icon: '/images/tech-icons/mysql.svg' },
+  { name: 'Vercel', icon: '/images/tech-icons/vercel.svg' },
+  { name: 'CSS', icon: '/images/tech-icons/css.svg' },
+  { name: 'HTML', icon: '/images/tech-icons/html.svg' },
+  { name: 'Docker', icon: '/images/tech-icons/docker.svg' },
+];
+
+export type { Technology };
